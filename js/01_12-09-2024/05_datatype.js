d36be8b79750d4098ef00d3d31dd72b1fc9f189e
urvashi <urvashi.d.bhavsar@gmail.com>
2024-09-12 15:19:33 +0530

    12th changes

---
+++ js/01_12-09-2024/05_datatype.js
@@ -0,0 +1,25 @@
+var a = 10;
+console.log(a);
+
+a = 12.22
+console.log(a);
+
+a = "Hello"
+console.log(a);
+
+a = true;
+console.log(a);
+
+var b;
+console.log(b);
+
+b = null;
+console.log(b);
+
+b = ""
+console.log(b);
+
+
+
+
+
---
+++ js/01_12-09-2024/script.js
@@ -0,0 +1,2 @@
+// document.write("Hello world")
+console.log("Hello world");
