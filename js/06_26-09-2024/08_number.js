a1eee1d607a21e9600701771f4798175eb7a602a
urvashi <urvashi.d.bhavsar@gmail.com>
2024-09-26 14:26:37 +0530

    26th changes

---
+++ js/06_26-09-2024/08_number.js
@@ -0,0 +1,25 @@
+let n1 = 12, n2 = 14.44;
+
+console.log(Number.isFinite(n1));
+console.log(Number.isFinite(n2));
+
+console.log(Number.isInteger(n1));
+console.log(Number.isInteger(n2));
+
+console.log(Number.parseFloat(n1));
+console.log(Number.parseFloat(n2));
+
+console.log(Number.parseInt(n1));
+console.log(Number.parseInt(n2));
+
+console.log(n1.toExponential());
+console.log(n2.toExponential());
+
+console.log(n1.toFixed(5));
+console.log(n2.toFixed(5));
+
+console.log(n1.toPrecision(5));
+console.log(n2.toPrecision(5));
+
+console.log(n1.toString());
+console.log(n2.toString());
---
+++ js/06_26-09-2024/10_string.js
@@ -0,0 +1,26 @@
+var str = "JavaScript Language, TypeScript Language"
+
+console.log(str.charAt(2));
+console.log(str.charCodeAt(1));
+console.log(str.concat(" is Script Language"));
+console.log(str.indexOf("a"));
+console.log(str.lastIndexOf("a"));
+console.log(str.search("Script"));
+console.log(str.match("Script"));
+console.log(str.replace("JavaScript", "TypeScript"));
+console.log(str.substring(2, 12));
+console.log(str.slice(2, 12));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.split(","));
+var newstr = '           user          '
+console.log(newstr.valueOf());
+console.log(newstr.trim());
+
+
+
+
+
+
+
+
